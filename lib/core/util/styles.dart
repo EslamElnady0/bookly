f6eb9e7da461916a2e3190fb623146453d0ca3bc
eslam <eslamelnady254@gmail.com>
2024-06-13 22:58:00 +0300

    added some text styles

--- lib/core/util/styles.dart
+++ lib/core/util/styles.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles {
-  static TextStyle titleMedium =
-      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600);
+  static TextStyle textStyle18 =
+      TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600);
+  static TextStyle textStyle20 = TextStyle(
+      fontSize: 20.sp,
+      fontWeight: FontWeight.normal,
+      fontFamily: "GT Sectra Fine");
+  static TextStyle textStyle30 = TextStyle(
+      fontSize: 30.sp,
+      fontWeight: FontWeight.normal,
+      fontFamily: "GT Sectra Fine");
+  static TextStyle textStyle14 =
+      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.normal);
+  static TextStyle textStyle16 =
+      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500);
 }
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/util/styles.dart';
 import 'package:bookly/features/home/presentation/views/widget/cutom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widget/featured_books_list_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,8 +14,8 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
+        const CustomAppBar(),
+        const FeaturedBooksListView(),
         SizedBox(
           height: 50.h,
         ),
@@ -22,10 +23,42 @@ class HomeViewBody extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 12.w),
           child: Text(
             "Best Seller",
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
-        )
+        ),
+        const BestSellerListViewItem()
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.w),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 120.h,
+            child: AspectRatio(
+              aspectRatio: 3.2 / 5,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.r),
+                    color: Colors.red,
+                    image: const DecorationImage(
+                        image: AssetImage(
+                          "assets/images/test_image.png",
+                        ),
+                        fit: BoxFit.fill)),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
