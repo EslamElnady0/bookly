2cd21a176df5f4410e73e2421f3a5ec8c5a7999f
eslam <eslamelnady254@gmail.com>
2024-06-13 13:17:24 +0300

    beeb beeeb

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -10,6 +10,8 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset("assets/images/Logo.png"),
+
+        ///
         const SizedBox(
           height: 5,
         ),
