f466e86f3d8653ee6d79800ce0c1c810545f6a4c
eslam <eslamelnady254@gmail.com>
2024-06-13 22:21:34 +0300

    installed google fonts + changed font family

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 
@@ -18,8 +19,10 @@ class BooklyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       builder: (context, child) {
         return GetMaterialApp(
-          theme:
-              ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+          theme: ThemeData.dark().copyWith(
+              scaffoldBackgroundColor: kPrimaryColor,
+              textTheme:
+                  GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
           debugShowCheckedModeBanner: false,
           home: const SplashView(),
         );
