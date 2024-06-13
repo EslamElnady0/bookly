c97fcbe4422e32067d11ca5cdb2ec65749c97fae
eslam <eslamelnady254@gmail.com>
2024-06-13 14:52:19 +0300

    created custom listview item

---
+++ lib/features/home/presentation/views/widget/custom_list_view_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 280.h,
+      child: AspectRatio(
+        aspectRatio: 3 / 5,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.r),
+              color: Colors.red,
+              image: const DecorationImage(
+                  image: AssetImage(
+                    "assets/images/test_image.png",
+                  ),
+                  fit: BoxFit.fill)),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/features/home/presentation/views/widget/custom_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widget/cutom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -7,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [CustomAppBar(), CustomListViewItem()],
     );
   }
 }
