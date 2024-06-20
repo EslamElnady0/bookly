fb7babcd9ab1d114329a81772c3c4aedb78f67fd
eslam <eslamelnady254@gmail.com>
2024-06-20 13:33:09 +0300

    cached network imaged added

--- lib/features/home/presentation/views/widget/custom_book_image.dart
+++ lib/features/home/presentation/views/widget/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -7,17 +8,14 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16.r),
-            image: DecorationImage(
-                image: NetworkImage(
-                  imageUrl,
-                ),
-                fit: BoxFit.fill)),
-      ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16.r),
+      child: AspectRatio(
+          aspectRatio: 2.7 / 4,
+          child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: BoxFit.fill,
+              errorWidget: (context, url, error) => const Icon(Icons.error))),
     );
   }
 }
